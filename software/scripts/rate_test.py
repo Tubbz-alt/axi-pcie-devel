a350973ad1a9a20c5ec130b599c10b8506e8788a
Ryan Herbst <rherbst@slac.stanford.edu>
2020-01-21 12:44:28 -0800

    Save state

--- software/scripts/rate_test.py
+++ software/scripts/rate_test.py
@@ -3,9 +3,6 @@
 import pyrogue
 
 pyrogue.addLibraryPath('../../../firmware/submodules/axi-pcie-core/python')
-pyrogue.addLibraryPath('../../../firmware/submodules/clink-gateway-fw-lib/python')
-pyrogue.addLibraryPath('../../../firmware/submodules/lcls-timing-core/python')
-pyrogue.addLibraryPath('../../../firmware/submodules/lcls2-pgp-fw-lib/python')
 pyrogue.addLibraryPath('../../../firmware/submodules/surf/python')
 pyrogue.addLibraryPath('../../../firmware/python')
 pyrogue.addLibraryPath('../python')
@@ -14,15 +11,15 @@ import pyrogue.pydm
 import pyrogue.gui
 import rogue
 
-from RateTestApp import RateTestRoot
+from AxiPcieDevelApp.RateTest import RateTestRoot
 
 rogue.Logging.setFilter('pyrogue.prbs.rx',rogue.Logging.Debug)
 #rogue.Logging.setLevel(rogue.Logging.Debug)
 
 with RateTestRoot() as root:
 
-    #pyrogue.pydm.runPyDM(root=root)
+    pyrogue.pydm.runPyDM(root=root)
     #pyrogue.gui.runGui(root=root)
-    pyrogue.waitCntrlC()
+    #pyrogue.waitCntrlC()
 
 
