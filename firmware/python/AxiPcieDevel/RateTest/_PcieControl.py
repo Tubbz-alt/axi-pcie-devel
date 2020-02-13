7f5ed44e7be7c6d6da5f35d4f1b51f83e9394ef3
Ryan Herbst <rherbst@slac.stanford.edu>
2020-02-13 09:25:26 -0800

    Update

--- firmware/python/AxiPcieDevel/RateTest/_PcieControl.py
+++ firmware/python/AxiPcieDevel/RateTest/_PcieControl.py
@@ -15,16 +15,16 @@ class PcieControl(pr.Device):
 
         self._data   = [None] * 4
         self._prbsRx = [None] * 4
-        self._prbsTx = [None] * 2
+        self._prbsTx = [None] * 4
 
         for i in range(4):
-            self._data[i]   = rogue.hardware.axi.AxiStreamDma(f'/dev/datagpu_{index}',(i+4)*256,True)
+            self._data[i]   = rogue.hardware.axi.AxiStreamDma(f'/dev/datagpu_{index}',i*256,True)
             self._prbsRx[i] = pyrogue.utilities.prbs.PrbsRx(name=f'PrbsRx[{i}]', width=256)
             self.add(self._prbsRx[i])
 
             pyrogue.streamConnect(self._data[i],self._prbsRx[i])
 
-        for i in range(2):
+        for i in range(4):
             self._prbsTx[i] = pyrogue.utilities.prbs.PrbsTx(name=f'PrbsTx[{i}]', width=256)
             self.add(self._prbsTx[i])
 
